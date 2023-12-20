b3ae7bf152e224b59b552f10541f618e3763f807
Queenen <AnnBjo66962@stud.noroff.no>
2023-12-20 11:45:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const openLoginFormBtn = document.querySelector("#openLoginForm");
 const loginForm = document.querySelector("#loginForm");
 const initialBtns = document.querySelector("#initialBtns");
+const usernameInput = document.querySelector("#floatingInputUsername");
 
 openLoginFormBtn.addEventListener("click", () => {
   loginForm.classList.remove("d-none");
   initialBtns.classList.add("d-none");
+});
+
+loginForm.addEventListener("submit", () => {
+  sessionStorage.setItem("username", usernameInput.value);
 });

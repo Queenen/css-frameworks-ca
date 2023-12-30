0426aaec7e78f585cdb23ba7d890f8c5b0e50333
Queenen <AnnBjo66962@stud.noroff.no>
2023-12-30 14:36:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const openLoginFormBtn = document.querySelector("#openLoginForm");
 const loginForm = document.querySelector("#loginForm");
 const initialBtns = document.querySelector("#initialBtns");
 const usernameInput = document.querySelector("#floatingInputUsername");
+const filterBtn = document.querySelector("#filterButton");
+const filterOptions = document.querySelector("#filterPosts");
 
 openLoginFormBtn.addEventListener("click", () => {
   loginForm.classList.remove("d-none");
